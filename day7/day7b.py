f2b5194efdd8b49b6eaf971859fc58cf061ab71e
jo-van-do <78650799+jo-van-do@users.noreply.github.com>
2024-12-07 17:05:01 +0100

    day7

---
+++ day7/day7b.py
@@ -0,0 +1,62 @@
+import itertools
+import operator
+import copy
+
+equations = []
+
+with open('day7\data7.txt', 'r') as f:
+    for line in f.read().split('\n'):
+        answer = int(line.split(': ')[0])
+        nums = [int(num) for num in line.split(': ')[1].split(' ')]
+        equations.append([answer, nums])
+
+f.close()
+
+
+def generate_equations(target,nums):    
+    ops = { "+": operator.add, "*": operator.mul, "||": operator.concat}
+    correct = False
+    total = 0
+    
+    n_symbols = len(nums)-1
+    symbols = [x for x in itertools.product(['*','+','||'], repeat=n_symbols)]
+    
+    for i in range(len(symbols)):
+        if correct == True:
+            break
+        
+        else:
+        
+            nums_copy = copy.deepcopy(nums)
+        
+            for j in range(len(nums_copy)-1):
+                
+                if symbols[i][j] == '||':
+                    answer = ops[symbols[i][j]] ( str(nums_copy[j]) , str(nums_copy[j+1]) )
+                    answer = int(answer)
+                else:
+            
+                    answer = ops[symbols[i][j]](nums_copy[j],nums_copy[j+1])
+                    
+                nums_copy[j+1] = answer
+     
+            if answer == target:
+                correct = True
+                total = answer
+                break
+        
+    return total
+
+
+total_sum = 0
+count = 0
+
+for line in equations:
+    print('line:', count)
+    count+= 1
+    total_sum += generate_equations(line[0],line[1])
+
+print(total_sum)
+
+
+
